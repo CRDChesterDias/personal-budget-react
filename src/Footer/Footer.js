21a0848967cb4c1f65e02fa56bcf799c73c3bdd0
CRDChesterDias <dias.chester.r@hotmail.com>
2020-10-13 22:21:00 -0400

    Brought PBufget code in

---
+++ src/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bottom">
+        <footer className="center">
+            All rights reserved &copy; Fabio Nolasco
+        </footer>
+    </footer>
+  );
+}
+
+export default Footer;
